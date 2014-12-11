ae12fddb8a08863214cbdcdbfb0e28dcd8cdd20a
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-11 09:39:49 -0800

    Use gem version directly

--- lib/carnivore-files/version.rb
+++ lib/carnivore-files/version.rb
@@ -1,9 +1,6 @@
 module Carnivore
   module Files
-    # Customized version class
-    class Version < Gem::Version
-    end
     # Current version of library
-    VERSION = Version.new('0.1.0')
+    VERSION = Gem::Version.new('0.1.0')
   end
 end
